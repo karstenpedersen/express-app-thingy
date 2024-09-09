6609f34bb4ce588c0a7e022c34bcb56e4f8db58f
Karsten Pedersen <karstenfp.all@gmail.com>
2024-09-09 16:11:21 +0200

    Setup project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { getSupabase } from "./supabase";
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT ?? 8080;
+
+app.get('/', function (req: Request, res: Response) {
+    const supabase = getSupabase();
+    const max = 100;
+    const num = Math.floor(Math.random() * max);
+    res.send({
+        num
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ src/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+
+export function getSupabase() {
+    const url = process.env.SUPABASE_URL;
+    const key = process.env.SUPABASE_KEY;
+    if (!url || !key) {
+        return null;
+    }
+
+    return createClient(url, key);
+}
